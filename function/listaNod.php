c6cc3fc2fea49976df94a127e0fa2b3a5ce683fc
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-04 11:38:52 +0100

    NEW VERSION

--- function/listaNod.php
+++ function/listaNod.php
@@ -1,35 +1,4 @@
 <?php
-
-/*---------------------- SELECT DATA ---------------------------*/
-function selectData($selectSQL,$connection)
-{
-	$option=(string)"";
-	$first=(boolean)TRUE;
-	$selected='selected="selected"';
-	$i=(integer)0;
-	$j=(integer)0;
-        $tablicaWynik=array();
-        $connection->query($selectSQL,'1');
-    	$data=$connection->queryReturnValue();
-        echo "<pre>";
-        //print_r($data);
-        echo "</pre>";
-        $num_rows = count($data);
-	
-	if ($num_rows > 0) 
-	{
-                foreach($data as $klucz=> $wartosc)
-		{
-                    //echo "i - ${i} , ${klucz} , ".$wartosc['nod']," <br/>";
-                    $tablicaWynik[$klucz][$klucz]=$wartosc['nod'];
-                    $j++;
-		}
-		$i++;
-		$j=0;
-	};
-
-    return (array)$tablicaWynik;
-}
 function createOption($tabDane)
 {
 // Pracownie nazwa
@@ -40,11 +9,11 @@ $tmpNazwa=(string)"";
 
 	foreach($tabDane as $klucz =>$wartosc)
 	{
-
 		foreach($wartosc as $kluczTab => $wartoscTab)
 		{
-			if($kluczTab=='id') $tmpId=$wartoscTab; 
-			if($kluczTab=='nazwa' || $kluczTab=='nod') $tmpNazwa=$wartoscTab;
+                    //echo "kluczTab - ".$kluczTab." - ".$wartoscTab."<br/>";
+                    if($kluczTab=='id') $tmpId=$wartoscTab; 
+                    if($kluczTab=='nazwa' || $kluczTab=='nod') $tmpNazwa=$wartoscTab;
 		};
 		$option.="<option value=\"".$tmpId."|".$tmpNazwa."\">".$tmpNazwa."</option>";
 	};
@@ -52,9 +21,12 @@ return $option;
 }
 $tablicaPrac=array();
 $tablicaKlaster=array();
-$tablicaKlaster=selectData("SELECT id,nod,pracownia FROM klaster WHERE 1=? group by id",$dbLink);
-$tablicaPrac=selectData("SELECT id,nazwa FROM pracownia WHERE WSK_U=? ORDER BY id ",$dbLink);
+$dbLink->query('SELECT id,nod,pracownia FROM klaster WHERE 1=? group by id','1');
+$tablicaKlaster= $dbLink->queryReturnValue();
+
+$dbLink->query('SELECT id,nazwa FROM pracownia WHERE WSK_U=? ORDER BY id','0');
+$tablicaPrac=$dbLink->queryReturnValue();
+
 $optionKlaster=(string)createOption($tablicaKlaster);
 $optionPrac=(string)createOption($tablicaPrac);
-//mysqli_close($connection);
 ?>

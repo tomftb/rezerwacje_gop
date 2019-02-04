703018f7693a59a041cc2ed120def8aa2f65d2aa
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-04 10:24:34 +0100

    NEW VERSION

--- function/listaNod.php
+++ function/listaNod.php
@@ -1,83 +1,34 @@
 <?php
 
 /*---------------------- SELECT DATA ---------------------------*/
-function selectData($selectSQL,$connection="")
+function selectData($selectSQL,$connection)
 {
 	$option=(string)"";
 	$first=(boolean)TRUE;
 	$selected='selected="selected"';
 	$i=(integer)0;
 	$j=(integer)0;
-	if($_SESSION["PHPV"]<7.0)
-	{	
-		$result = mysqli_query($connection,$selectSQL) or die ('Zapytanie zakończone niepowodzeniem: ' . mysqli_error($connection));
-		$num_rows = mysqli_num_rows($result);
-	}
-	else
-	{
-		$result = mysqli_query($connection,$selectSQL) or die ('Zapytanie zakończone niepowodzeniem: ' . mysqli_error($connection));
-		$num_rows = mysqli_num_rows($result);
-	}	
+        $tablicaWynik=array();
+        $connection->query($selectSQL,'1');
+    	$data=$connection->queryReturnValue();
+        echo "<pre>";
+        //print_r($data);
+        echo "</pre>";
+        $num_rows = count($data);
+	
 	if ($num_rows > 0) 
 	{
-		if($_SESSION["PHPV"]<7.0)
+                foreach($data as $klucz=> $wartosc)
 		{
-			while ($wynikSQL = mysqli_fetch_array($result, MYSQLI_ASSOC))
-			{
-				/*
-				IF($_SESSION["username"]=="tborczynski")
-				{
-					echo "[id] $wynikSQL[0] |";
-					echo "[nod] - ".$wynikSQL[1]." | ";
-					echo "[pracownia] - ".[2]."</br>";
-				};
-				*/
-				//$option.="<option class=\"OPT_NOD\" value=".$wynikSQL['id'].">".$wynikSQL['nod']."</option>";
-				foreach($wynikSQL as $klucz=> $wartosc)
-				{
-					$tablicaWynik[$i][$klucz]=$wartosc;
-					$j++;
-				}
-				$i++;
-				$j=0;
-			};//WHILE
-		}//IF PHP 7
-		else
-		{
-			while ($wynikSQL = mysqli_fetch_array($result, MYSQLI_ASSOC))
-			{
-				/*
-				IF($_SESSION["username"]=="tborczynski")
-				{
-					echo "[id] ".$wynikSQL['id']." |";
-					echo "[nod] - ".$wynikSQL['nod']." | ";
-					echo "[pracownia] - ".$wynikSQL['pracownia']."</br>";
-				};
-				*/
-				foreach($wynikSQL as $klucz =>$wartosc)
-				{
-					$tablicaWynik[$i][$klucz]=$wartosc;
-					$j++;
-				}
-				$i++;
-				$j=0;
-			};//WHILE
-			
-		};//ELSE
+                    //echo "i - ${i} , ${klucz} , ".$wartosc['nod']," <br/>";
+                    $tablicaWynik[$klucz][$klucz]=$wartosc['nod'];
+                    $j++;
+		}
+		$i++;
+		$j=0;
 	};
-/*
-echo "[".basename(__FILE__)."]Wynik SQL</br>";
-foreach($tablicaWynik as $klucz=> $wartosc)
-{
-	echo "$klucz - ";
-	foreach($wartosc as $klucz2 => $wartosc2)
-	{
-		echo $wartosc2.",";
-	};
-	echo "</br>";
-};
-*/
-return (array)$tablicaWynik;
+
+    return (array)$tablicaWynik;
 }
 function createOption($tabDane)
 {
@@ -94,19 +45,15 @@ $tmpNazwa=(string)"";
 		{
 			if($kluczTab=='id') $tmpId=$wartoscTab; 
 			if($kluczTab=='nazwa' || $kluczTab=='nod') $tmpNazwa=$wartoscTab;
-			//echo $kluczTab.": ".$wartoscTab.", ";
-			
 		};
 		$option.="<option value=\"".$tmpId."|".$tmpNazwa."\">".$tmpNazwa."</option>";
-		//echo "</br>";
-		
 	};
 return $option;
 }
 $tablicaPrac=array();
 $tablicaKlaster=array();
-$tablicaKlaster=selectData("SELECT `id`,`nod`,`pracownia` FROM `klaster` ORDER BY `id`",$connection);
-$tablicaPrac=selectData("SELECT `id`,`nazwa` FROM `pracownia` WHERE `WSK_U`=0 ORDER BY `id` ",$connection);
+$tablicaKlaster=selectData("SELECT id,nod,pracownia FROM klaster WHERE 1=? group by id",$dbLink);
+$tablicaPrac=selectData("SELECT id,nazwa FROM pracownia WHERE WSK_U=? ORDER BY id ",$dbLink);
 $optionKlaster=(string)createOption($tablicaKlaster);
 $optionPrac=(string)createOption($tablicaPrac);
 //mysqli_close($connection);

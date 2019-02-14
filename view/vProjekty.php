00241ac8a6585cd12f9afdcebb8dc6ebdfbac3d8
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-14 11:07:36 +0100

    new version

--- view/vProjekty.php
+++ view/vProjekty.php
@@ -74,11 +74,9 @@
                 $lastIdExtraDoc=0;
                 if($condition[0]==='t')
                 {
-                    echo '<input type="text" class="form-control" name="'.$value[2].'" id="'.$value[2].'" placeholder="Pole wymagane" onblur="parseValue(this,\''.$value[2].'\')"/>';
+                    echo '<input type="text" class="form-control" name="'.$value[2].'" id="'.$value[2].'" placeholder="Pole wymagane" onblur="parseFieldValue(this,\''.$value[2].'\')"/>';
                      echo '<div class="text-left w-100 mb-0 mt-1">
-                <div class="alert alert-danger mb-0" id="errDiv-'.$value[2].'" style="display: none;">
-                <span id="errText-'.$value[2].'"></span>
-            </div>  
+                <div class="alert alert-danger mb-0" id="errDiv-'.$value[2].'" style="display: none;"></div>  
             </div>';
                 }
                 else if($condition[0]==='d')
@@ -103,7 +101,6 @@
      ?>
             <div class="form-group row mt-0" style="border:0px solid red;">
                 <div class="col-sm-12" id="additionalDoc">
-                    
                 </div>
             </div>
        <div class="form-group row mt-0" style="border:0px solid red;">
@@ -117,7 +114,6 @@
                 </div>
             </div>
             <div class="col-sm-4" id="extraFormDocButton">
-                
                 </div>
        </div> 
     <div class="form-group row mb-0" >
@@ -127,10 +123,9 @@
      </div>
     </form>
     </div>
-        <div class="modal-footer w-100 mt-0 mb-0 pb-0 pl-0 pr-0" style="border:0px solid red">
-            <div class=" w-100 mb-0" style="border:0px solid black">
-                <div class="mb-1 " style="border:0px solid orange">
-                    <hr class="mt-1 mb-1"></hr>
+        <div class="modal-footer w-100 mt-0 mb-0 pb-0 pl-0 pr-0">
+            <div class=" w-100 mb-0">
+                <div class="mb-1">
                         <small class="modal-title text-left text-secondary pl-1 pb-2" id="fieldModalLabel">Legenda: </small> 
                         <ul class="text-secondary font-weight-normal small" style="list-style-type:square;">
                             <li>A - litera alfabetu, C - cyfra</li>
@@ -145,7 +140,7 @@
                             <li>Dodatkowe dokumenty - max 50 znaków</li>
                         </ul>
                 </div>
-                <div class="alert alert-danger mb-0" id="errDiv-overall" style="display: none; border:0px solid green">
+                <div class="alert alert-danger mb-0" id="errDiv-overall" style="display: none;">
                     <span id="errText-overall"></span>
                 </div>
             </div>
@@ -156,37 +151,35 @@
 <!-- ADAPTED MODAL PROJECT -->
 <div class="modal fade " id="ProjectAdaptedModal" tabindex="-1" role="dialog" aria-labelledby="ProjectAdaptedModalContent" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <div class="modal-content" style="border:0px solid red">
           <div class="modal-header" id="ProjectAdaptedBgTitle">
             <h2 class="modal-title" id="fieldModalLabel"><span class="text-white" id="ProjectAdaptedTextTitle">USUWANIE PROJEKTU:</span></h2> 
                 <button type="button" class="close mr-0" data-dismiss="modal" aria-label="Close">
                     <i class="fa fa-times" aria-hidden="true"></i>
                 </button>
             </div>
-            <div class="modal-body mb-0 pb-1 pt-1" id="ProjectAdaptedBodyContent">
-                <div class="form-group row" style="border:0px solid green" id="ProjectAdaptedBodyContentTitle">
+            <div class="modal-body mb-0 pb-0 pt-0 mt-0" id="ProjectAdaptedBodyContent" style="border:0px solid blue">
+                <div class="form-group row mb-1 pb-0  pt-0 mt-0" style="border:0px solid green" id="ProjectAdaptedBodyContentTitle">
                     <div class=" col-sm-12 mt-4" >
                         <h5 class="modal-title text-center" id="fieldModalLabel"><span id="projectTitle"></span></h5> 
-                        
                     </div>
                 </div>
-                <div class="form-group row" >
+                <div class="form-group row mb-0 pb-0" style="border:0px solid orange" >
                     <div class="col-sm-12"  id="ProjectAdaptedDynamicData">
                     </div>
                 </div>
-                <div class="form-group row" >
+                <div class="form-group row mb-0" style="border:1px solid black" >
                     <div class=" col-sm-12" id="ProjectAdaptedButtonsBottom">
-                        
                     </div>
                 </div>
             </div>
-            <div class="modal-footer w-100 mt-0" >
-                <div class="text-left w-100">
-                    <div class=" row" style="border:0px solid black">
-                        <small class="modal-title text-left ml-1 text-secondary" id="fieldModalLabel">Project id: <span id="projectId"></span></small> 
-                        <small class="modal-title text-left ml-1 text-secondary" id="fieldModalLabel2"><span id="projectId2"></span></small> 
+            <div class="modal-footer w-100 mt-1" >
+                <div class="w-100 mr-0 ml-0 pr-0 pl-0" style="border:0px solid purple">
+                     <div class="row w-100" style="border:0px solid black">
+                        <small class="text-left text-secondary" id="fieldModalLabel">Project id: <span id="projectId"></span></small> 
+                        <small class="text-left text-secondary" id="fieldModalLabel2"><span id="projectId2"></span></small> 
                     </div>
-                    <div class="alert alert-danger row" id="errDiv-Adapted-overall" style="display: none;" style="border:0px solid red">
+                    <div class="alert alert-danger row" id="errDiv-Adapted-overall" style="border:0px solid blue">
                         <span id="errText-Adapted-overall"></span>
                     </div>
                 </div>
@@ -197,7 +190,7 @@
 <!-- END ADAPTED MODAL PROJECT -->
 <div id="readroot" style="display: none">
         <div class="input-group" name="pdfExtra">
-            <input type="text" class="entry form-control" id="inputPdfDok" placeholder="" name="inputPdfDok" onblur="parseValue(this,'inputPdfDok')"/>            
+            <input type="text" class="entry form-control" id="inputPdfDok" placeholder="" name="inputPdfDok" onblur="parseFieldValue(this,'inputPdfDok')"/>            
                 <div class="input-group-addon input-group-append ">
                     <div class=" btn  btn-danger rounded-right" onclick="closeNode(this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode),this.parentNode.parentNode)">
                         <i class="fa fa-minus" aria-hidden="true"></i>
@@ -211,22 +204,40 @@
     </div>  
 </div>
 <!-- PROJECT DETAIL TEMPLATE -->
-<div class="modal fade" tabindex="-1" role="dialog" class="modal-content mb-0" id="addProjectModalDetail" aria-hidden="true">
+<div class="modal fade mb-0" tabindex="-1" role="dialog" id="addProjectModalDetail" aria-hidden="true">
 <div class="modal-body mb-0 pb-1 pt-1">
 <form class="form-horizontal"  autocomplete="off" method="POST"  ENCTYPE="multipart/form-data" action="javascript:void(0);" name="createPdfForm">
     <div class="form-group row mt-2 mb-1" id="addProjectModalDetailFields">		
     </div>
     <div class="form-group row mb-0" id="addProjectModalDetailButtons">
-       <div class=" col-sm-12" >
+       <div class=" col-sm-12" style="border:1px solid green;" >
            <div class="btn-group pull-right">
                 <button class="btn btn-dark" data-dismiss="modal">Anuluj</button>
                 <button id="postDataSubmit" class="btn btn-info" >Edytuj</button>
            </div>
        </div>
-     </div>	   
+     </div>
 </form>
 </div>
-
+<!-- LEGEND -->
+<div class="modal fade mb-0 pb-0" id="legendDiv" style="border:0px solid green;">
+        <hr class="w-100"></hr>
+                        <small class="modal-title text-left text-secondary pl-1 pb-2" id="fieldModalLabel">Legenda: </small> 
+                        <ul class="text-secondary font-weight-normal small" style="list-style-type:square;">
+                            <li>A - litera alfabetu, C - cyfra</li>
+                            <li>Numer,Temat,Dodatkowe dokumenty:</li>
+                                <ul style="list-style-type:disc;">
+                                <li>może się zaczą A|C</li>
+                                <li>może zawierać A,C,/,_,- i spacje</li>
+                                <li>może się zakończyć .</li>
+                                </ul>
+                            <li>Numer - max 20 znaków.</li>
+                            <li>Temat - max 100 znaków.</li>
+                            <li>Dodatkowe dokumenty - max 50 znaków</li>
+                        </ul>
+</div>    
+<!-- END LEGEND -->
+           
 </div>
 <!-- END PROJECT DETAIL TEMPLATE -->
 <div id="div-inputPdf7a" style="display:block; border:0px solid black;"></div>

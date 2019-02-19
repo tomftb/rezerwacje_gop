f6687c7441a9eca886eadc98d8b8c3b58b6ab19f
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-19 08:15:42 +0100

    new version

--- function/sendMail.php
+++ function/sendMail.php
@@ -1,85 +1,48 @@
 <?php
-//echo "SEND MAIL";
-//require 'modul/PHPMailer_v5.1/class.phpmailer.php';
-									//require 'modul/PHPMailer-master/src/Exception.php';
-									//include('./SMTP.php');
-									$quota=$_POST["rozmiarPlik"]*30;
-									$mailBody="Zarejestrowano zgłoszenie na utworzenie nowego projektu o specyfikacji:\n\nNazwa projektu\t\t-\t".$_POST["nazwaProjektu"]."\n";
-									$mailBody.="Rozmiar pliku bazowego\t- \t".$_POST["rozmiarPlik"]." ".$_POST["rozmiarJednostka"]."\n";
-									$mailBody.="Sugerowana quota\t- \t".$quota." ".$_POST["rozmiarJednostka"]."\n";
-									$mailBody.="Przypisani użytkownicy\t- \t".$_SESSION['nazwiskoImie'].", ".$_POST["przypisaniPracownicy"]."\n\n";
-									
-									$mailBody.="Zgłaszający\t\t- \t".$_SESSION['nazwiskoImie']." (".$_SESSION["mail"].") ";
-									
-									if($_SESSION["PHPV"]<7.0)
-									{
-										
-										//echo "PHP 5 <br/>";
-										if(checkFile("function/PHPMailer_v5.1/class.phpmailer.php")) include ("function/PHPMailer_v5.1/class.phpmailer.php");
-										$mail = new PHPMailer;
-									}
-									else
-									{
-										//echo "PHP 7 <br/>";
-										if(checkFile("function/PHPMailer-master/src/SMTP.php")) include ("function/PHPMailer-master/src/SMTP.php");
-										if(checkFile("function/PHPMailer-master/src/Exception.php")) include ("function/PHPMailer-master/src/Exception.php");
-										if(checkFile("function/PHPMailer-master/src/PHPMailer.php")) include ("function/PHPMailer-master/src/PHPMailer.php");
-										$mail = new PHPMailer\PHPMailer\PHPMailer();
-										
-										
-									};
-									if($_SESSION['username']=='tborczynski') echo "<p class=\"P_LOAD_FILE\"><span class=\"S_LOAD_OK\">Załadowano konfigurację PHPMailer dla  PHP</span> ".$_SESSION["PHPV"]."</p>";
-									//'tomasz.borczynski@geofizyka.pl'=>'Tomasz Borczyński'
-									if(checkFile(".cfg/email.php")) include (".cfg/email.php");
-									$conf['email'][$_SESSION["mail"]]= $_SESSION['nazwiskoImie'];
-									/*
-									echo "<pre>";
-									print_r($conf['email']);
-									echo "</pre>";
-									die('STOP');
-									*/
-									/* old V
-									$tabAddress=array(
-													
-													$_SESSION['mail'] => $_SESSION['nazwiskoImie']
-													
-													
-													);
-*/													
-													/* ADD
-													
-													,'michal.machtel@geofizyka.pl'=>'Michał Machtel'
-													,'zbigniew.szychulski@geofizyka.pl' => 'Zbigniew Szychulski'
-													,'seweryn.tlalka@geofizyka.pl' => 'Serweryn Tlałka'
-													
-													
-													--
-													,'jerzy.kisielewicz@geofizyka.pl' =>'Jerzy Kisielewicz'
-													*/
-										
-											$mail->IsSMTP();
-											$mail->Host = "smtp.geofizyka.pl";
-											$mail->CharSet = 'UTF-8';
-											$mail->setFrom('rezerwacje-gop@geofizyka.pl', 'rezerwacje-gop');
-											foreach($conf['email'] as $emailAdres => $emailUser)
-											{
-												$mail->addAddress($emailAdres, $emailUser);
-											}; // END FOREACH
-											$mail->Subject  = 'Zgłoszenie na utworzenie udziału dla Projektu';
-											$mail->Body     = $mailBody."\n\n--\nTa wiadomość została wygenerowana automatycznie, nie odpowiadaj na nią.";
-											if(!$mail->send())
-											{
-											  echo 'Zgłoszenie nie zostały wysłane.</br>';
-											  echo 'Mailer error: ' . $mail->ErrorInfo;
-											}
-											else
-											{
-												foreach($conf['email'] as $emailAdres => $emailUser)
-												{
-													echo '<p class="p_info_16_b" >Zgłoszenie zostało wysłane do - <span style="color:#000000;">'.$emailUser." (".$emailAdres.')</span></p>';
-												};
-											  
-											  
-											};
-										
-?>
+$rPlik=filter_input(INPUT_POST,"rozmiarPlik",FILTER_VALIDATE_INT);
+$nProjekt=filter_input(INPUT_POST,"nazwaProjektu");
+$rJednostka=filter_input(INPUT_POST,"rozmiarJednostka");
+$pPracownicy=filter_input(INPUT_POST,"przypisaniPracownicy");
+$quota=$rPlik*30;
+$mailBody="Zarejestrowano zgłoszenie na utworzenie nowego projektu o specyfikacji:\n\nNazwa projektu\t\t-\t".$nProjekt."\n";
+$mailBody.="Rozmiar pliku bazowego\t- \t".$rPlik." ".$rJednostka."\n";
+$mailBody.="Sugerowana quota\t- \t".$quota." ".$rJednostka."\n";
+$mailBody.="Przypisani użytkownicy\t- \t".$_SESSION['nazwiskoImie'].", ".$pPracownicy."\n\n";
+$mailBody.="Zgłaszający\t\t- \t".$_SESSION['nazwiskoImie']." (".$_SESSION["mail"].") ";		
+
+/*
+ * PHP 5
+ * if(checkFile("function/PHPMailer_v5.1/class.phpmailer.php")) include ("function/PHPMailer_v5.1/class.phpmailer.php");
+ * $mail = new PHPMailer;
+ * ACT PHP 7
+ * 
+ */
+if(checkFile("function/PHPMailer-master/src/SMTP.php")) include ("function/PHPMailer-master/src/SMTP.php");
+if(checkFile("function/PHPMailer-master/src/Exception.php")) include ("function/PHPMailer-master/src/Exception.php");
+if(checkFile("function/PHPMailer-master/src/PHPMailer.php")) include ("function/PHPMailer-master/src/PHPMailer.php");
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+if(checkFile(".cfg/email.php")) include (".cfg/email.php");
+$conf['email'][$_SESSION["mail"]]= $_SESSION['nazwiskoImie'];
+$mail->IsSMTP();
+$mail->Host = "smtp.geofizyka.pl";
+$mail->CharSet = 'UTF-8';
+$mail->setFrom('rezerwacje-gop@geofizyka.pl', 'rezerwacje-gop');
+foreach($conf['email'] as $emailAdres => $emailUser)
+{
+    $mail->addAddress($emailAdres, $emailUser);
+};
+$mail->Subject  = 'Zgłoszenie na utworzenie udziału dla Projektu';
+$mail->Body     = $mailBody."\n\n--\nTa wiadomość została wygenerowana automatycznie z portalu rezerwacjegop.geofizyka.pl, nie odpowiadaj na nią.";
+if(!$mail->send())
+{
+    echo 'Zgłoszenie nie zostały wysłane.</br>';
+    echo 'Mailer error: ' . $mail->ErrorInfo;
+}
+else
+{
+    foreach($conf['email'] as $emailAdres => $emailUser)
+    {
+	echo '<p class="p_info_16_b" >Zgłoszenie zostało wysłane do - <span style="color:#000000;">'.$emailUser." (".$emailAdres.')</span></p>';
+    }
+}						

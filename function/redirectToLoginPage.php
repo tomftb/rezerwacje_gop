0f51a45a5687817b8e28e837f681369ba3216f67
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-26 15:18:26 +0100

    change session['permission'] to session['perm']['LOG_INO_APP']

--- function/redirectToLoginPage.php
+++ function/redirectToLoginPage.php
@@ -1,5 +1,6 @@
 <?php
-if(!isset($_SESSION["permission"]) ||  $_SESSION["permission"]!=='yes')
+//if(!isset($_SESSION["permission"]) ||  $_SESSION["permission"]!=='yes')
+if(!isset($_SESSION["perm"]) && in_array('LOG_INTO_APP',$_SESSION['perm']))
 {
 ?>
 <script>window.location.replace(window.location.protocol+'//'+window.location.host+"/");</script>

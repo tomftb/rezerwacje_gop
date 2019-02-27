a724cc60ec9852d63602dea136b4b805620c6cb4
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-27 10:01:58 +0100

    new version

--- function/checkPerm.php
+++ function/checkPerm.php
@@ -1,22 +1,36 @@
 <?php
 function checkPerm($perm,$permArray,$stop=1)
 {
-    $DOC_ROOT=filter_input(INPUT_SERVER,"DOCUMENT_ROOT");
     if(!in_array($perm,$permArray))
     {
-        echo '<div class="container" style="margin-top:100px;">';
-        echo '<div class="alert alert-danger row">';
-        echo "<span>[${perm}] BRAK DOSTĘPU</span>";
-        echo '</div>';
-        echo '</div>';
-        if($stop)
-        {
-            if(checkFile($DOC_ROOT.'/view/footer.php')) {include ($DOC_ROOT.'/view/footer.php');}
-            die(); 
-        }
-        else
-        {
-            return 0;
-        }
+        parseStop($stop);
+    }
+    else
+    {
+        return 1;
+    }
+}
+function showErr($perm)
+{
+    $DOC_ROOT=filter_input(INPUT_SERVER,"DOCUMENT_ROOT");
+    echo '<div class="container" style="margin-top:100px;">';
+    echo '<div class="alert alert-danger row">';
+    echo "<span>[${perm}] BRAK UPRAWNIENIA</span>";
+    echo '</div>';
+    echo '</div>'; 
+    if(checkFile($DOC_ROOT.'/view/footer.php')) {include ($DOC_ROOT.'/view/footer.php');}
+}
+function parseStop($perm,$stop)
+{
+    
+    if($stop)
+    {
+        showErr($perm);
+        die(); 
+    }
+    else
+    {
+        echo '<div class="container" style="margin-top:100px;"></div>';
+        return 0;
     }
 }

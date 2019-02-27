6c9b815a0740c174c02c75240efb864adc1b829e
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-27 09:58:18 +0100

    add redirection function

--- function/listaNod.php
+++ function/listaNod.php
@@ -1,26 +1,28 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/function/redirectToLoginPage.php");
+/*
+ * CREATE SELECT OPTION LIST
+ */
 function createOption($tabDane)
 {
-// Pracownie nazwa
-// Klaster nod
-$tmpId=(integer)0;
-$tmpNazwa=(string)"";
-	$option=(string)"";
+    // Pracownie nazwa
+    // Klaster nod
+    $tmpId=(integer)0;
+    $tmpNazwa=(string)"";
+    $option=(string)"";
 
-	foreach($tabDane as $klucz =>$wartosc)
-	{
-		foreach($wartosc as $kluczTab => $wartoscTab)
-		{
-                    //echo "kluczTab - ".$kluczTab." - ".$wartoscTab."<br/>";
-                    if($kluczTab=='id') $tmpId=$wartoscTab; 
-                    if($kluczTab=='nazwa' || $kluczTab=='nod') $tmpNazwa=$wartoscTab;
-		};
-		$option.="<option value=\"".$tmpId."|".$tmpNazwa."\">".$tmpNazwa."</option>";
-	};
-return $option;
+    foreach($tabDane as $wartosc)
+    {
+        foreach($wartosc as $kluczTab => $wartoscTab)
+        {
+            //echo "kluczTab - ".$kluczTab." - ".$wartoscTab."<br/>";
+            if($kluczTab=='id') {$tmpId=$wartoscTab;} 
+            if($kluczTab=='nazwa' || $kluczTab=='nod') {$tmpNazwa=$wartoscTab;}
+        }
+        $option.="<option value=\"".$tmpId."|".$tmpNazwa."\">".$tmpNazwa."</option>";
+    }
+    return $option;
 }
-$tablicaPrac=array();
-$tablicaKlaster=array();
 $dbLink->query('SELECT id,nod,pracownia FROM klaster WHERE 1=? group by id','1');
 $tablicaKlaster= $dbLink->queryReturnValue();
 
@@ -29,4 +31,3 @@ $tablicaPrac=$dbLink->queryReturnValue();
 
 $optionKlaster=(string)createOption($tablicaKlaster);
 $optionPrac=(string)createOption($tablicaPrac);
-?>

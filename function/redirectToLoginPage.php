9e9e3d7656518e8d9c8ba29c5ecb747c23454b1f
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-27 10:01:16 +0100

    new version

--- function/redirectToLoginPage.php
+++ function/redirectToLoginPage.php
@@ -1,10 +1,39 @@
 <?php
 //if(!isset($_SESSION["permission"]) ||  $_SESSION["permission"]!=='yes')
-if(!isset($_SESSION["perm"]) && in_array('LOG_INTO_APP',$_SESSION['perm']))
+if(isset($_SESSION))
 {
-?>
-<script>window.location.replace(window.location.protocol+'//'+window.location.host+"/");</script>
-<?php
-};
-?>
+    //echo "SESSION EXISTS<br/>";
+    if(isset($_SESSION['perm']))
+    {
+        //echo "SESSION PERM EXISTS<br/>";
+        if(in_array('LOG_INTO_APP',$_SESSION['perm']))
+        {
+            //echo "SESSION LOG_INTO_APP EXISTS<br/>";
+            //OK
+        }
+        else
+        {
+            echo '<div class="container" style="margin-top:100px;">';
+            echo '<div class="alert alert-danger row">';
+            echo "<span>[LOG_INTO_APP] BRAK DOSTĘPU</span>";
+            echo '</div>';
+            echo '</div>';
+            die();
+        }
+    }
+    else
+    {
+        redirect(); 
+    }
+}
+else
+{
+    //echo "NO SESSION<br/>";
+    redirect();
+}
+function redirect()
+{
+    echo "<script>window.location.replace(window.location.protocol+'//'+window.location.host);</script>";
+}
+//die();
 

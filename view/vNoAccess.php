4471f7e9836c1fde81af111fd315f0d5910e590e
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-27 07:59:45 +0100

    new view - no access

---
+++ view/vNoAccess.php
@@ -0,0 +1,11 @@
+<div class="alert alert-danger row" id="errDiv-Adapted-overall">
+    <span id="errText-Adapted-overall"></span>
+</div>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+

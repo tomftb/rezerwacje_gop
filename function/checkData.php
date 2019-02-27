7b74f92c7ad1bb0577b3801f48f9a5dbfc0f7e94
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-02-27 10:01:34 +0100

    add redirection function add checkPerm function

--- function/checkData.php
+++ function/checkData.php
@@ -1,24 +1,28 @@
 <?php
-if(ISSET($_POST["rezerwuj"]))
-{	//echo "Wcisnieto \"rezerwuj\"</br>";
-//echo "<p class=\"P_LOAD_FILE\"><span class=\"S_LOAD_OK\">Załadowano funkcję </span>- ".basename(__FILE__,'.php')."</p>";
-foreach($_POST as $id => $wartosc)
+require_once($_SERVER["DOCUMENT_ROOT"]."/function/redirectToLoginPage.php");
+if(checkPerm('EDIT_KLAST',$_SESSION['perm'],0))
 {
-	//echo "[$id] $wartosc</br>";
-};
-$dataNod0=(array)explode('|',$_POST["nod0"]);
-$dataNod1=(array)explode('|',$_POST["nod1"]);
-$dataPrac=(array)explode('|',$_POST["pracownia"]);
-$updateNodValue="";
-if($dataNod0[0] <= $dataNod1[0])
-{
-    $updateNodValue=$dataPrac[0].",".$dataNod0[0].",".$dataNod1[0];
+    if(ISSET($_POST["rezerwuj"]))
+    {	//echo "Wcisnieto \"rezerwuj\"</br>";
+        //echo "<p class=\"P_LOAD_FILE\"><span class=\"S_LOAD_OK\">Załadowano funkcję </span>- ".basename(__FILE__,'.php')."</p>";
+        foreach($_POST as $id => $wartosc)
+        {
+                //echo "[$id] $wartosc</br>";
+        }
+        $dataNod0=(array)explode('|',$_POST["nod0"]);
+        $dataNod1=(array)explode('|',$_POST["nod1"]);
+        $dataPrac=(array)explode('|',$_POST["pracownia"]);
+        $updateNodValue="";
+        if($dataNod0[0] <= $dataNod1[0])
+        {
+            $updateNodValue=$dataPrac[0].",".$dataNod0[0].",".$dataNod1[0];
+        }
+        else
+        {
+                $updateNodValue=$dataPrac[0].",".$dataNod1[0].",".$dataNod0[0];
+        }
+        $updateNod="UPDATE klaster SET pracownia=? WHERE id>=? AND id<=? ";
+        $dbLink->query($updateNod,$updateNodValue);
+    }
 }
-else
-{
-        $updateNodValue=$dataPrac[0].",".$dataNod1[0].",".$dataNod0[0];
-};
-    $updateNod="UPDATE klaster SET pracownia=? WHERE id>=? AND id<=? ";
-    $dbLink->query($updateNod,$updateNodValue);
-};
-?>
+

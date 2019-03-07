713b04bfdd4d8873cac5395768546f43ad6ad579
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-07 15:12:01 +0100

    new v inclue db email parameters

--- function/sendMail.php
+++ function/sendMail.php
@@ -1,4 +1,7 @@
 <?php
+require_once($DOC_ROOT.'/function/checkFile.php');
+if(checkFile($DOC_ROOT.'/.cfg/config.php')) include_once($DOC_ROOT.'/.cfg/config.php');
+
 $rPlik=filter_input(INPUT_POST,"rozmiarPlik",FILTER_VALIDATE_INT);
 $nProjekt=filter_input(INPUT_POST,"nazwaProjektu");
 $rJednostka=filter_input(INPUT_POST,"rozmiarJednostka");
@@ -20,29 +23,110 @@ $mailBody.="Zgłaszający\t\t- \t".$_SESSION['nazwiskoImie']." (".$_SESSION["mai
 if(checkFile("function/PHPMailer-master/src/SMTP.php")) include ("function/PHPMailer-master/src/SMTP.php");
 if(checkFile("function/PHPMailer-master/src/Exception.php")) include ("function/PHPMailer-master/src/Exception.php");
 if(checkFile("function/PHPMailer-master/src/PHPMailer.php")) include ("function/PHPMailer-master/src/PHPMailer.php");
+
+
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 
-if(checkFile(".cfg/email.php")) include (".cfg/email.php");
-$conf['email'][$_SESSION["mail"]]= $_SESSION['nazwiskoImie'];
-$mail->IsSMTP();
-$mail->Host = "smtp.geofizyka.pl";
-$mail->CharSet = 'UTF-8';
-$mail->setFrom('rezerwacje-gop@geofizyka.pl', 'rezerwacje-gop');
-foreach($conf['email'] as $emailAdres => $emailUser)
+if(checkFile(".cfg/email.php")){ include (".cfg/email.php");};
+
+//die('stop email');
+
+
+//GET EMAIL PARAMETERS
+$dbLink->query('SELECT SKROT,WARTOSC FROM parametry WHERE UPPER(SKROT) LIKE (?) ','MAIL_%');
+
+class EMAIL
 {
-    $mail->addAddress($emailAdres, $emailUser);
-};
-$mail->Subject  = 'Zgłoszenie na utworzenie udziału dla Projektu';
-$mail->Body     = $mailBody."\n\n--\nTa wiadomość została wygenerowana automatycznie z portalu rezerwacjegop.geofizyka.pl, nie odpowiadaj na nią.";
-if(!$mail->send())
+    private $flattenParm=array();
+    private $parm=array();
+    private $data=array();
+    public function setData($data)
+    {
+       
+        //$this->data=$data;
+        //$this->parseFlattenParm($this->data);
+        $this->parseParm($data);
+    }
+    // REKURENCYJNIE - UZYSKAMY FLATTEN
+    private function parseFlattenParm($data)
+    {
+        foreach($data as $key => $value)
+        {
+            if(is_array($value))
+            {
+                $this->parseParm($value);
+            }
+            else
+            {
+                array_push($this->flattenParm,$value);
+                //UNSET($data[$key]);
+            }
+        }
+    }
+    private function parseParm($data)
+    {
+        $tmpArray=array();
+        foreach($data as $key => $value)
+        {
+            $this->parm[$value['SKROT']]=$value['WARTOSC'];
+            //$tmpArray[$key]=$value;
+        }
+        //$this->parm=$tmpArray;
+    }
+    public function flattenParm()
+    {
+        return ($this->parm);
+    }
+}
+$parseParm=NEW EMAIL();
+$parseParm->setData($dbLink->queryReturnValue());
+$EMAIL=$parseParm->flattenParm();
+/* */
+echo "<pre>";
+print_r($EMAIL);
+echo "</pre>";
+set_time_limit(10);
+try
+{
+    
+    $mail->IsSMTP();
+    $mail->Timeout  =   5;
+    $mail->Host = $EMAIL['MAIL_SRV'];
+    $mail->CharSet = $EMAIL['MAIL_CHARSET'];
+    $mail->Password = $EMAIL['MAIL_PASS'];
+    $mail->SMTPKeepAlive = true;
+    if($EMAIL['MAIL_PASS']!='')
+    {
+        $mail->SMTPAuth = true; 
+    }
+    $mail->Port = $EMAIL['MAIL_PORT_OUT']; 
+    $from=explode('@',$EMAIL['MAIL_USER']);
+    //$mail->setFrom('rezerwacje-gop@geofizyka.pl', 'rezerwacje-gop');
+    $mail->setFrom($EMAIL['MAIL_USER'], $from[0]);
+    $recipient=explode(';',$EMAIL['MAIL_RECIPIENT']);
+    //print_r($recipient);
+    if($EMAIL['MAIL_RECV'])
+    {
+        array_push($recipient,$_SESSION['mail']);
+    }
+    //foreach($recipient as $emailAdres => $emailUser)
+    foreach($recipient as $emailAdres)
+    {
+        //$mail->addAddress($emailAdres, $emailUser);
+        $mail->addAddress($emailAdres);
+    }
+    $mail->Subject  = 'Zgłoszenie na utworzenie udziału dla Projektu';
+    $mail->Body     = $mailBody."\n\n--\nTa wiadomość została wygenerowana automatycznie z portalu rezerwacjegop.geofizyka.pl, nie odpowiadaj na nią.";
+    $mail->send();
+    $mail->SmtpClose(); 
+    foreach($recipient as $emailAdres )
+    {
+        echo '<p class="p_info_16_b" >Zgłoszenie zostało wysłane do - <span style="color:#000000;">'.$emailAdres.'</span></p>';
+    }
+}
+catch (Exception $e)
 {
     echo 'Zgłoszenie nie zostały wysłane.</br>';
     echo 'Mailer error: ' . $mail->ErrorInfo;
 }
-else
-{
-    foreach($conf['email'] as $emailAdres => $emailUser)
-    {
-	echo '<p class="p_info_16_b" >Zgłoszenie zostało wysłane do - <span style="color:#000000;">'.$emailUser." (".$emailAdres.')</span></p>';
-    }
-}						
+					

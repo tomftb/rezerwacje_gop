d7724826e93892bc76d13b083590fc5b37722c5c
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-08 08:05:01 +0100

    add find button

--- view/vRole.php
+++ view/vRole.php
@@ -1,7 +1,7 @@
 <?php require_once(filter_input(INPUT_SERVER,"DOCUMENT_ROOT")."/function/redirectToLoginPage.php"); ?>
 <?php require_once(filter_input(INPUT_SERVER,"DOCUMENT_ROOT")."/modul/mValidUrl.php");?>
 <body>
-<div class="w-100 " style="margin-top:-55px;position:fixed;">
+<div class="w-100 "  style="margin-top:-55px;position:fixed; z-index:996;">
     <div class="row">
         <div class=" col-sm-2" >
             <div class="btn pull-left mt-0" > 
@@ -12,6 +12,14 @@
             <h2 class="text-center mb-3 mt-1 text-info">Role :</h2>
         </div>
         <div class=" col-sm-2" >
+             <div class="row ">
+                <div class="sm-col-8">
+                    <h5 class="text-right mt-2 text-secondary" >Szukaj : </h5>
+                </div>
+                <div class="sm-col-4">
+                    <input class="form-control mt-1" onchange="getAjaxData('getAllRoleLike','&filter='+this.value,'sRole',null)"/>
+                </div>
+            </div>
         </div>
     </div>
 </div>

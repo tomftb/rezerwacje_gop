75ee559cdba1fee94b64cc00e97628d0d3bc5a5b
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-14 13:43:51 +0100

    fix header

--- view/vParametry.php
+++ view/vParametry.php
@@ -3,19 +3,19 @@
 <body>
 <div class="w-100 " style="margin-top:-55px;position:fixed; z-index:996;">
     <div class="row">
-        <div class=" col-sm-2" >
+        <div class=" col-sm-4" >
             
         </div>
-        <div class=" col-sm-8" >
+        <div class=" col-sm-4" >
             <h2 class="text-center mb-3 mt-1 text-info">Parametry :</h2>
         </div>
-        <div class=" col-sm-2" >
-            <div class="row ">
+        <div class=" col-sm-4" >
+            <div class="row float-right mr-4">
                 <div class="sm-col-8">
-                    <h5 class="text-right mt-2 text-secondary" >Szukaj : </h5>
+                    <h5 class="mt-2 text-secondary" >Szukaj : </h5>
                 </div>
                 <div class="sm-col-4">
-                    <input class="form-control mt-1" onchange="getAjaxData('getAllParmLike','&filter='+this.value,'sParm',null)"/>
+                    <input class="ml-1 form-control mt-1" onchange="getAjaxData('getAllParmLike','&filter='+this.value,'sParm',null)"/>
                 </div>
             </div>
         </div>

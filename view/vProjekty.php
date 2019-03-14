2c0bff22e83a7ede8369d9ed7c653d7ac135b92c
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-14 13:44:31 +0100

    fix header

--- view/vProjekty.php
+++ view/vProjekty.php
@@ -3,21 +3,21 @@
 <body>
 <div class="w-100 " style="margin-top:-55px; position:fixed; z-index:996;">
     <div class="row">
-        <div class=" col-sm-2" >
+        <div class=" col-sm-4" >
             <div class="btn pull-left mt-0" > 
                  <button id='addNewProjectButton' class="btn btn-info pull-right mr-0 mb-0 mt-0 ml-1"  data-toggle="modal" data-target="#ProjectAdaptedModal" onclick="createAdaptedModal('createProject','','','n')">Dodaj projekt</button>
             </div> 
         </div>
-        <div class=" col-sm-8" >
+        <div class=" col-sm-4">
             <h2 class="text-center mb-3 mt-1 text-info">Projekty :</h2>
         </div>
-        <div class=" col-sm-2" >
-            <div class="row ">
+        <div class=" col-sm-4">
+            <div class="row float-right mr-4">
                 <div class="sm-col-8">
-                    <h5 class="text-right mt-2 text-secondary" >Szukaj : </h5>
+                    <h5 class="mt-2 text-secondary" >Szukaj : </h5>
                 </div>
                 <div class="sm-col-4">
-                    <input class="form-control mt-1" onchange="getAjaxData('getprojectslike','','','&filter='+this.value,null)"/>
+                    <input class="ml-1 form-control mt-1" onchange="getAjaxData('getprojectslike','','','&filter='+this.value,null)"/>
                 </div>
             </div>
         </div>

baffac39cd5362900889ba0ee9cdbe201355c762
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-14 13:44:07 +0100

    fix header

--- view/vUprawnienia.php
+++ view/vUprawnienia.php
@@ -3,18 +3,18 @@
 <body>
 <div class="w-100 " style="margin-top:-55px;position:fixed; z-index:996;">
     <div class="row">
-        <div class=" col-sm-2" >
+        <div class=" col-sm-4" >
         </div>
-        <div class=" col-sm-8" >
+        <div class=" col-sm-4" >
             <h2 class="text-center mb-3 mt-1 text-info">Uprawnienia :</h2>
         </div>
-        <div class=" col-sm-2" >
-            <div class="row ">
+        <div class=" col-sm-4" >
+            <div class="row float-right mr-4">
                 <div class="sm-col-8">
-                    <h5 class="text-right mt-2 text-secondary" >Szukaj : </h5>
+                    <h5 class="mt-2 text-secondary" >Szukaj : </h5>
                 </div>
                 <div class="sm-col-4">
-                    <input class="form-control mt-1" onchange="getAjaxData('getAllPermLike','&filter='+this.value,'sPermissions',null)"/>
+                    <input class="ml-1 form-control mt-1" onchange="getAjaxData('getAllPermLike','&filter='+this.value,'sPermissions',null)"/>
                 </div>
             </div>
         </div>

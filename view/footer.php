db73e1884ba83df34e9c1a69218c6d7f0beaed47
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-15 07:02:53 +0100

    change col size oc each block

--- view/footer.php
+++ view/footer.php
@@ -1,16 +1,16 @@
 <footer class="sticky-footer fixed-bottom bg-dark">
     <div class="row">
-        <div class="col-sm-3">
+        <div class="col-sm-5">
             <div class="text-left ml-2">
                 <small class="text-white">Zalogowano - <?php echo $_SESSION["username"];?> <span class="text-info"><?php echo " (".$_SESSION["mail"].")"; ?></span></small>
             </div>
         </div>
-  <div class="col-sm-6">
+  <div class="col-sm-2">
       <div class="text-center">
         <small class="text-white">GT: Rezerwacje GOP</small>
     </div>
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm-5">
         <div class="text-right  mr-2">
             <small class="text-white">Autor: Tomasz Borczyński</small>
         </div>

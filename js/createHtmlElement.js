24b9e3691850b9121bbca674461212e86ce71ab7
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-19 08:19:58 +0100

    new js create html tag

---
+++ js/createHtmlElement.js
@@ -0,0 +1,79 @@
+// FUNCTION CREATE ANY HTML ELEMENT
+// a. html tag to setup
+// b. array of array to setup tag attribute
+// c. array of classes
+// d. array of css
+function createHtmlElement(htmlTag,elementAttribute,elementClassList,elementStyle)
+{
+    //console.log('---createHtmlElement()---\n'+htmlTag);
+    //console.log(elementAttribute);
+    //console.log(elementClassList);
+    //console.log(elementStyle);
+    var htmlElement=document.createElement(htmlTag);
+    var i=0;
+    // ASSIGN Attribute
+    if(elementAttribute!==null && elementAttribute!==undefined)
+    {
+        for(j=i;j<elementAttribute.length;j++)
+        {
+            htmlElement.setAttribute(elementAttribute[j][0],elementAttribute[j][1]);
+        };
+    }
+    // ASSIGN ADDITIONAL CLASS
+    if(elementClassList!==null && elementClassList!==undefined)
+    {
+        for(j=i;j<elementClassList.length;j++)
+        {
+            htmlElement.classList.add(elementClassList[j]);
+        };
+    };
+    // ASSIGN STYLES
+    if(elementStyle!==null && elementStyle!==undefined)
+    {
+         for(j=i;j<elementStyle.length;j++)
+        {
+            //console.log(elementStyle[j][0]);
+            //console.log(elementStyle[j][1]);
+            //htmlElement.style.elementStyle[j][0] = elementStyle[j][1];
+            htmlElement=addStyleToHtmlTag(htmlElement,elementStyle[j][0],elementStyle[j][1]);
+        };
+    }
+    //console.log(htmlElement);
+    return (htmlElement);
+}
+function addStyleToHtmlTag(htmlElement,styleName,styleValue)
+{
+    switch(styleName)
+    {
+        case 'border':
+            htmlElement.style.border=styleValue;
+            break;
+        case 'backgroundColor':
+            htmlElement.style.backgroundColor=styleValue;
+            break;
+        case 'borderColor':
+            htmlElement.style.borderColor=styleValue;
+            break;
+        case 'color':
+            htmlElement.style.color=styleValue;
+            break;
+        case 'borderTopRightRadius':
+            htmlElement.style.borderTopRightRadius=styleValue;
+            break;
+        case 'borderBottomRightRadius':
+            htmlElement.style.borderBottomRightRadius=styleValue;
+            break;
+        case 'borderTopLeftRadius':
+            htmlElement.style.borderTopLeftRadius=styleValue;
+            break;
+        case 'borderBottomLeftRadius':
+            htmlElement.style.borderBottomLeftRadius=styleValue;
+            break;
+        case 'display':
+            htmlElement.style.display=styleValue;
+            break;
+        default:
+            break;
+    };
+    return(htmlElement);
+}

5034bbdd56189477c560f9f151e186b83390ff0a
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2019-03-20 13:56:14 +0100

    new v

--- modul/mValidUrl.php
+++ modul/mValidUrl.php
@@ -1,6 +1,7 @@
 <?php
 $HTTP_HOST=filter_input(INPUT_SERVER,"HTTP_HOST");
 $HTTP = (filter_input(INPUT_SERVER,"HTTPS") ? "HTTPS://" : "HTTP://"); 
+$PORT=filter_input(INPUT_SERVER,"SERVER_PORT");
 $URL=$HTTP.$HTTP_HOST;
 ?>
 

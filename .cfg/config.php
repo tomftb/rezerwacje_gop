78ae2c30d359c344a0d670a6f349be09fcb8ea32
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2020-10-23 10:35:24 +0200

    end of refactor

--- .cfg/config.php
+++ .cfg/config.php
@@ -1,5 +1,5 @@
 <?php
-require_once(filter_input(INPUT_SERVER,"DOCUMENT_ROOT")."/function/databaseManagement.php");
+require_once(filter_input(INPUT_SERVER,"DOCUMENT_ROOT")."/class/dbManage.php");
 class initialDb extends dbManage
 {
     private $dbParm=array
@@ -8,21 +8,25 @@ class initialDb extends dbManage
                 'db'=>'rezerwacjegop',
                 'port'=>3306,
                 'user'=>'rezerwacjegop',
-                'pass'=>'5DPbvuNiRbyoUBTN',
-                'logLvl'=>0
+                'pass'=>'NURQYnZ1TmlSYnlvVUJUTg==',
+                'logLvl'=>0,
+                'cipher'=>'y'
             );
     private $dbLink="";
-
     function __construct()
-    {
-        $this->dbLink=parent::__construct($this->dbParm['host'],$this->dbParm['db'],$this->dbParm['port'],$this->dbParm['user'],$this->dbParm['pass'],$this->dbParm['logLvl']);
+    {   
+        $this->dbLink=parent::__construct($this->dbParm['host'],$this->dbParm['db'],$this->dbParm['port'],$this->dbParm['user'],$this->dbParm['pass'],$this->dbParm['logLvl'],$this->dbParm['cipher']);
+        $this->log(2,"[".__METHOD__."]");
     }		
     public function getDbLink()
     {
+        $this->log(0,"[".__METHOD__."]");
         return $this->dbLink;
     }
     function __destruct()
-    {}
+    {
+        $this->log(2,"[".__METHOD__."]");
+    }
 }
-$dbLink=NEW initialDb();
-$dbLink->getDbLink();
+//$dbLink=NEW initialDb();
+//$dbLink->getDbLink();

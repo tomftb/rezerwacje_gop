fb4343569fdc5d19d8ba8749696345b3fe938003
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-02-09 12:52:59 +0100

    new v

--- view/infoHeader.php
+++ view/infoHeader.php
@@ -31,4 +31,48 @@
             </div>
         </div>
 </header><!-- #header -->
-<SCRIPT type="text/javascript" src="<?php echo $URL; ?>/js/headerView.js"></SCRIPT>
+<SCRIPT type="text/javascript" src="<?=$URL?>/js/headerView.js<?=uniqid()?>"></SCRIPT>
+<!-- ADAPTED MODAL PROJECT -->
+<div class="modal fade" id="AdaptedModal" tabindex="-1" role="dialog" aria-labelledby="AdaptedModalContent" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header" id="AdaptedBgTitle">
+            <h2 class="modal-title" id="fieldModalLabel"><span class="text-white" id="AdaptedTextTitle"></span></h2> 
+                <button type="button" class="close mr-0" data-dismiss="modal" aria-label="Close" id="closeModal" onclick="reloadData(defaultTask);">
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                </button>
+            </div>
+            <div class="modal-body mb-0 pb-0 pt-0 mt-0" id="AdaptedBodyContent">
+                <div class="form-group row mb-1 pb-0  pt-0 mt-0" id="AdaptedBodyContentTitle">
+                    <div class=" col-sm-12 mt-4" >
+                        <h5 class="modal-title text-center" id="fieldModalLabel">
+                            <span id="projectTitle">
+                            </span>
+                        </h5> 
+                    </div>
+                </div>
+                <div class="form-group row mb-0 pb-0">
+                    <div class="col-sm-12"  id="AdaptedDynamicData">
+                    </div>
+                </div>
+                <div class="form-group row mb-0">
+                    <div class="col-sm-12" >
+                        <div class="btn-group pull-right" id="AdaptedButtonsBottom">
+                        </div>
+                    </div>
+                </div>
+                    <div class="alert alert-danger row mt-1 mb-0" id="errDiv-Adapted-overall" style="display: none;">
+                    </div>
+                <div class="form-group row" id="AdaptedBodyExtra">
+                </div>
+            </div>
+            <div class="modal-footer w-100 mt-1" >
+                <div class="w-100 mr-0 ml-0 pr-0 pl-0">
+                     <div class="row w-100" id="AdaptedModalInfo">           
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>   
+<!-- END ADAPTED MODAL PROJECT -->

5e2486cd211479603b89f0a8631b63fcd85d0807
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-02-09 13:36:37 +0100

    new v

--- js/headerView.js
+++ js/headerView.js
@@ -1,43 +1,92 @@
+var log=false;
+
 function setMenuActive()
 {
-    removeClass();
-    addClass(parseUrlId());
+    if(log) {console.log('setMenuActive()');};
+    var id=parseUrlId();
+    removeClassFromEle(id);
+    addClass(id);
 }
 function parseUrlId()
 {
+    if(log) {console.log('parseUrlId()');};
     var currentLocation = window.location;
     var res = currentLocation.search.split("=");
-    return res[1];
+     /* set default */
+    var id=1;
+    if(log) {console.log(res.length);};
+    if(res.length>1)
+    {
+        id=parseInt(res[1],10);
+    }
+    if(isNaN(id))
+    {
+        if(log) {console.log('isNaN');};
+        id=1;
+    }
+    if(log) {console.log('ID TO RETURN => '+id);};
+    return id;
 }
 function addClass(id)
 {
-    //console.log(id);
-    if(id==null)
-    {
-        id=1;   
-    }
+    if(log) {console.log('addClass()\nid => '+id);};
     // SET ADMINISTRATOR
     if(id>=5)
     {
-        
         document.getElementById("li-5").setAttribute("class", "menu-active");
     }
     document.getElementById("li-"+id).setAttribute("class", "menu-active"); 
-    //console.log(document.getElementById("li-"+id));
 }
-function removeClass()
+function removeClassFromEle(id)
 {
-    for (i = 1; i < 4; i++)
+    if(log) {console.log('removeClassFromEle()\nid => '+id);};
+
+    for (var i = 1; i < 5; i++)
     { 
-        try
+        if(log) {console.log(i);};
+        if(id!==i)
         {
-           document.getElementById("li-"+i).removeAttribute("class"); 
-        }
-        catch(err)
-        {
-            console.log(err.message);
-        }
+            var ele=document.getElementById("li-"+i);
+            if(log) {console.log(ele);};
+            removeClass(ele);   
+        };    
+    }; 
+}
+function removeClass(ele)
+{
+    if(log)
+    {
+        console.log('removeClass()');
+        console.log(ele);
     };
-    
+    if(typeof(ele) !== 'undefined' && ele !== null)
+    {
+        
+        removeAttribute(ele,'class');
+    }
+    else
+    {
+        if(log)
+        {
+            console.log('element is null or undefined');
+            console.log(ele);
+        };
+    }
+}
+function removeAttribute(ele,atr)
+{
+    if(log){
+        console.log('removeAttribute()');
+        console.log(ele);
+    }
+    if(ele.hasAttribute(atr))
+    {
+        ele.removeAttribute(atr); 
+    }
+    else
+    {
+        /* nothing to do */
+        if(log) {console.log("NO "+atr+" attribute");};
+    }
 }
 setMenuActive();

25055107f20f2b840d3f7d3a110797b428770a00
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-05-05 08:34:08 +0200

    add $_SESSION["uid"]= uniqid();

--- class/validLogin.php
+++ class/validLogin.php
@@ -10,30 +10,24 @@ class validLogin
     private $dbLink;
     private $logLink;
     
-    public function __construct()
-    {
+    public function __construct(){
         $this->logLink=NEW logToFile();
         $this->userName=filter_input(INPUT_POST,"username");
         $this->userPassword=filter_input(INPUT_POST,"password");
     }
-    public function checkLoginData()
-    {
+    public function checkLoginData(){
         $this->logLink->log(0,"[".__METHOD__."]");
-        if($this->checkGet())
-        {
+        if($this->checkGet()){
             return 0;
         }
-        else if(isset($_SESSION["perm"]) && in_array('LOG_INTO_APP',$_SESSION['perm']))
-        {
+        else if(isset($_SESSION["perm"]) && in_array('LOG_INTO_APP',$_SESSION['perm'])){
             $this->logLink->log(0,"[".__METHOD__."] SESSION ALREADY INITIALISED");
             return 1;
         }
-        else if((trim($this->userName)==='') && (trim($this->userPassword)===''))
-        {
+        else if((trim($this->userName)==='') && (trim($this->userPassword)==='')){
             return 0;
         }
-        else
-        {
+        else{
             $this->dbLink=NEW initialDb();
             if(!self::getUserData()) {return 0;}
             if(!self::checkUserData()) {return 0;}
@@ -43,16 +37,13 @@ class validLogin
         }
         return 1;
     }
-    private function checkGet()
-    {
+    private function checkGet(){
         $this->logLink->log(0,"[".__METHOD__."]");
         $this->getLogout = filter_input(INPUT_GET, 'logout');
-        if($this->getLogout==='t')
-        {
+        if($this->getLogout==='t'){
             $this->info='Wylogowano.';
             $this->bgColor='bg-success';
-            if(!session_unset())
-            {
+            if(!session_unset()){
                 $this->logLink->log(0,"[".__METHOD__."] session_unset NOT WORK");
                 return 0;
             }
@@ -60,15 +51,12 @@ class validLogin
         }
         return 0;
     }
-    public function getInfoValue()
-    {
+    public function getInfoValue(){
         return $this->info;
     }
-    public function getBgColorValue()
-    {
+    public function getBgColorValue(){
         return $this->bgColor;
     }
-
     private function checkLoginInAD(){
         $this->logLink->log(0,"[".__METHOD__."]");
         /* GET FROM CONST */
@@ -82,15 +70,12 @@ class validLogin
         self::setError($ldap->getError(),'');
         return 0;
     }
-    private function checkLogin()
-    {
+    private function checkLogin(){
         $this->logLink->log(0,"[".__METHOD__."] ACCOUNT TYPE => ".$this->userData['typ']);
         $this->userData['typ']=intval($this->userData['typ'],10);
         /* TO DO => GET AVALIABLE ACCOUNT TYPE */
         /* CHECK ACCOUNT TYPE */
-        /* OLD => if($this->userData['typ']==='l') */
-        if($this->userData['typ']===2)
-        {
+        if($this->userData['typ']===2){
             /* 
              * LOCAL ACCOUNT => CHECK PASSWORD 
              * password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
@@ -103,7 +88,6 @@ class validLogin
             $_SESSION["mail"]=$this->userData['email'];
             $_SESSION["nazwiskoImie"]=$this->userData['nazwisko'].' '.$this->userData['imie']; 
         }
-        /* OLD V => else if($this->userData['typ']==='a'){ */
         else if($this->userData['typ']===1){
             /* AD ACCOUNT => CHECK IN AD */
             if(!self::checkLoginInAD()) {return 0;}
@@ -115,27 +99,22 @@ class validLogin
         }
         return 1;
     }
-    private function setSessionData()
-    {
+    private function setSessionData(){
         $_SESSION["username"]=$this->userName;
         $_SESSION["userid"]=$this->userData['id'];
-        $_SESSION["perm"]=$this->userData['perm'];     
+        $_SESSION["perm"]=$this->userData['perm'];  
+        $_SESSION["uid"]= uniqid();
     }
     private function getUserData()
     {
         $this->logLink->log(0,"[".__METHOD__."]");
         $sqlData[':login']=array($this->userName,'STR');
-        try
-	{
+        try{
             $this->dbLink->newQuery("SELECT `id`,`imie`,`nazwisko`,`email`,`wsk_u`,`typ`,`haslo`,`id_rola` FROM `uzytkownik` WHERE `login`=:login",$sqlData);
             $this->userData=$this->dbLink->getSth()->fetch(PDO::FETCH_ASSOC);
-            
-            
-            
             $this->logLink->logMulti(2,$this->userData,__METHOD__);
 	}
-	catch (PDOException $e)
-	{
+	catch (PDOException $e){
             self::setError('[ERROR] Wystąpił krytyczny błąd aplikacji! Skontaktuj się z Administratorem!',"[".__METHOD__."] Wystąpił błąd zapytania bazy danych: ".$e->getMessage());
             return 0;
 	}
@@ -143,32 +122,27 @@ class validLogin
     }
     private function checkUserData(){
         $this->logLink->log(0,"[".__METHOD__."]");
-        if(!is_array($this->userData))
-        {
+        if(!is_array($this->userData)){
             self::setError('Brak uprawnienia do zalogowania się.','');
             return 0;
         }
         return 1;
     }
-    private function getUserPerm()
-    {
+    private function getUserPerm(){
         $this->logLink->log(0,"[".__METHOD__."]");
         $this->setActSessionPermRole();
-        if(!in_array('LOG_INTO_APP',$this->userData['perm']))
-        {
+        if(!in_array('LOG_INTO_APP',$this->userData['perm'])){
             self::setError('[LOG_INTO_APP] Brak uprawnienia do zalogowania się','');
             return 0;
         }
         $this->logLink->log(0,"[".__METHOD__."] Uprawnienie `LOG_INTO_APP` istnieje, loguje...");
         return 1;
     }
-    protected function setActSessionPermRole()
-    {
+    protected function setActSessionPermRole(){
         $this->logLink->log(0,"[".__METHOD__."]");
         $sqlData[':id_rola']=array(intval($this->userData['id_rola'],10),'INT');
         $sqlDataPerm[':id']=array(intval($this->userData['id'],10),'INT');
-        try
-	{
+        try{
             /* ROLE PERM */
             $this->dbLink->newQuery("SELECT `SKROT` FROM v_upr_i_slo_rola_v2 WHERE `idRola`=:id_rola",$sqlData);
             $this->userData['rolePerm']=$this->dbLink->getSth()->fetchAll(PDO::FETCH_ASSOC);
@@ -180,31 +154,25 @@ class validLogin
             /* COMBINE PERM */
             self::combinePerm();
 	}
-	catch (PDOException $e)
-	{
+	catch (PDOException $e){
             self::setError('[ERROR] Wystąpił krytyczny błąd aplikacji! Skontaktuj się z Administratorem!',"[".__METHOD__."] Wystąpił błąd zapytania bazy danych: ".$e->getMessage());
             return 0;
 	}
     }
-    private function combinePerm()
-    {
+    private function combinePerm(){
         $this->logLink->log(0,"[".__METHOD__."]");
         $this->userData['perm']=array();
-        foreach($this->userData['rolePerm'] as $v)
-        {
+        foreach($this->userData['rolePerm'] as $v){
             array_push($this->userData['perm'],$v['SKROT']);
         }
-        foreach($this->userData['userPerm'] as $v)
-        {
-            if(!in_array($v['SKROT'],$this->userData['perm']))
-            {
+        foreach($this->userData['userPerm'] as $v){
+            if(!in_array($v['SKROT'],$this->userData['perm'])){
                 array_push($this->userData['perm'],$v['SKROT']);
             }
         }
         $this->logLink->logMulti(2,$this->userData['perm'],__METHOD__."::COMBINE PERM");
     }
-    private function setError($userError='',$log='')
-    {
+    private function setError($userError='',$log=''){
         $this->logLink->log(0,$log);
         $this->info=$userError;
         $this->bgColor='bg-danger';

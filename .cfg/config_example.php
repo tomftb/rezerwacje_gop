94bc0bee36bf634c9948422b718f2f5fc8acc3b9
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-06-28 10:57:02 +0200

    example config file

---
+++ .cfg/config_example.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	RENAME TO config.php
+*/
+define('dbParm',array
+            (
+                'host'=>'127.0.0.1',
+                'db'=>'db_name',
+                'port'=>3306,
+                'user'=>'db_username',
+                'pass'=>'db_password',
+                'logLvl'=>0,
+                'cipher'=>'y => base64 or n'
+            ));
+define('ldapParm',array
+            (
+                'host'=>'host',
+                'filter'=>'filter',
+                'tree'=>'tree',
+                'port'=>389,
+                'user'=>'ldap_user',
+                'password'=>'ldap_password'
+            ));
+//5DPbvuNiRbyoUBTN

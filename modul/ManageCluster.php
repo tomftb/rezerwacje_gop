272d66c6a887e27b0cc9725a0adf4c16b89c43f2
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-06-29 14:12:44 +0200

    new modul

---
+++ modul/ManageCluster.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ManageCluster
+ *
+ * @author tborczynski
+ */
+class ManageCluster {
+    //put your code here
+}
+/*
+ * <?php
+if(!defined("DR")){
+    die('Direct access not permitted');
+}
+else{
+    require_once(DR."/function/redirectToLoginPage.php");
+    require_once(DR."/.cfg/config.php");
+} 
+$dbLink=NEW initialDb();
+/*
+ * CREATE SELECT OPTION LIST
+ */
+/*
+function createOption($tabDane)
+{
+    // Pracownie nazwa
+    // Klaster nod
+    $tmpId=(integer)0;
+    $tmpNazwa=(string)"";
+    $option=(string)"";
+
+    foreach($tabDane as $wartosc)
+    {
+        foreach($wartosc as $kluczTab => $wartoscTab)
+        {
+            //echo "kluczTab - ".$kluczTab." - ".$wartoscTab."<br/>";
+            if($kluczTab=='id') {$tmpId=$wartoscTab;} 
+            if($kluczTab=='nazwa' || $kluczTab=='nod') {$tmpNazwa=$wartoscTab;}
+        }
+        $option.="<option value=\"".$tmpId."|".$tmpNazwa."\">".$tmpNazwa."</option>";
+    }
+    return $option;
+}
+$dbLink->query('SELECT id,nod,pracownia FROM klaster WHERE 1=? group by id','1');
+$tablicaKlaster= $dbLink->queryReturnValue();
+
+$dbLink->query('SELECT id,nazwa FROM pracownia WHERE WSK_U=? ORDER BY id','0');
+$tablicaPrac=$dbLink->queryReturnValue();
+
+$optionKlaster=(string)createOption($tablicaKlaster);
+$optionPrac=(string)createOption($tablicaPrac);
+
+ */

86ce448f4c9b10c97d4976103f5b44ec50aa69e6
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-06-29 13:50:54 +0200

    new RezerwujKlaster page

--- app_pages/RezerwujKlaster.php
+++ app_pages/RezerwujKlaster.php
@@ -4,7 +4,7 @@ final class RezerwujKlaster extends Page{
     private $Log;
     private $mainPerm='LOG_INTO_PROJ';
     private $view=['vRezerwujKlaster.php'];
-    private $js=['projekty_raport.js','projekty.js','projekty_zespol.js'];
+    private $js=[];
 
 	
     public function __construct(){

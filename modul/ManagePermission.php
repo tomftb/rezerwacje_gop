fd50eebb9c617a5ade7babf3069f987ca4320563
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-07-01 14:50:16 +0200

    new version

--- modul/ManagePermission.php
+++ modul/ManagePermission.php
@@ -3,7 +3,6 @@ class ManagePermission
 {
     private $inpArray=array();
     private $idPerm=0;
-    protected $responseType='POST';
     private $Log;
     private $dbLink;
     function __construct()
@@ -12,31 +11,38 @@ class ManagePermission
         $this->dbLink=LoadDb::load();
         $this->Log->log(0,"[".__METHOD__."]");
         $this->utilities=NEW Utilities();
-        $this->response=NEW Response('Permission');
     }
     public function uPermUsers()
     {
         $this->Log->log(0,"[".__METHOD__."]");
         $this->inpArray=filter_input_array(INPUT_POST);
         $this->Log->logMulti(0,$this->inpArray);
-        if(!array_key_exists('ID', $this->inpArray))
-        {
-            $this->response->setError(1,'KEY ID NOT FOUND');
-        }
+        
+        $this->utilities->keyExist($this->inpArray,'ID');
+        $this->utilities->isEmptyKeyValue($this->inpArray,'ID',true,1);
         $this->idPerm=$this->inpArray['ID'];
         UNSET($this->inpArray['ID']);
         /* check perrmission exist */
         self::checkPermissionExist();
         /* REMOVE NOT SENDED USERS */
-        $perm=$this->query('SELECT uu.`id_uzytkownik` as `idu` FROM `uzyt_i_upr` uu, `uzytkownik` u WHERE uu.`id_uzytkownik`=u.`id` AND u.`wsk_u`=? AND `id_uprawnienie`=?','0,'.$this->idPerm);       
-        array_map(array($this, 'deleteUserFromPerm'),$perm);
-        /* ADD SENDED USERS */
-        self::addUserToPerm(); 
-        return($this->response->setResponse(__METHOD__,'','cModal','POST'));     
+        try{
+            $this->dbLink->beginTransaction();
+            $this->dbLink->query('SELECT uu.`id_uzytkownik` as `idu` FROM `uzyt_i_upr` uu, `uzytkownik` u WHERE uu.`id_uzytkownik`=u.`id` AND u.`wsk_u`="0" AND `id_uprawnienie`=:i',[':i'=>[$this->idPerm,'INT']]);       
+            //$perm=;
+            //var_dump($perm);
+            array_map(array($this, 'deleteUserFromPerm'),$this->dbLink->sth->fetchAll(PDO::FETCH_ASSOC));
+            /* ADD SENDED USERS */
+            self::addUserToPerm(); 
+            $this->dbLink->commit();  
+        }
+        catch (PDOException $e){
+            $this->dbLink->rollback();
+            Throw New Exception("[".__METHOD__."] Wystąpił błąd zapytania bazy danych: ".$e->getMessage(),1);
+        }
+        $this->utilities->jsonResponse(__METHOD__,'','cModal','POST');    
     }
     private function deleteUserFromPerm($v)
     {
-        if($this->response->getError()!=='') { return false;}
         $this->Log->log(0,"[".__METHOD__."]");
         $this->Log->logMulti(0,$v);
         $found=false;
@@ -54,17 +60,19 @@ class ManagePermission
         if(!$found)
         {
             $this->Log->log(0,"[".__METHOD__."] USER (".$v['idu'].") NOT FOUND IN INPUT ARRAY => DELETE");
-            $this->query('DELETE FROM `uzyt_i_upr` WHERE `id_uzytkownik`=? AND id_uprawnienie=?',$v['idu'].",".$this->idPerm);
+            $sql=[
+                ':ip'=>[$this->idPerm,'INT'],
+                ':iu'=>[$v['idu'],'INT']
+            ];
+            $this->dbLink->query('DELETE FROM `uzyt_i_upr` WHERE `id_uzytkownik`=:iu AND id_uprawnienie=:ip',$sql);   
         }
     }
     private function addUserToPerm()
     {
-        if($this->response->getError()!=='') { return false;}
         $this->Log->log(0,"[".__METHOD__."]");
         foreach($this->inpArray as $id => $user)
         {
-            if(!self::addUser($id,$user))
-            {
+            if(!self::addPerm($id,$user)){
                 break;
             }
         }
@@ -72,18 +80,14 @@ class ManagePermission
     private function checkUserExist($idUser=0)
     {
         $this->Log->log(0,"[".__METHOD__."]");
-        $userData=$this->query('SELECT `id`,`wsk_u` FROM `uzytkownik` WHERE `id`=?',$idUser);
-        $count=count($userData);
-        $this->Log->logMulti(0,$userData);
-        $this->Log->log(0,"COUNT => ".$count);
-        if($count===0)
-        {
+        $this->dbLink->query('SELECT `id`,`wsk_u` FROM `uzytkownik` WHERE `id`=:i',[':i'=>[$idUser,'INT']]);
+        $userData=$this->dbLink->sth->fetch(PDO::FETCH_ASSOC);
+        if(!is_array($userData)){
             return false;
         }
-        $wsku=intval($userData[0]['wsk_u'],10);
+        $wsku=intval($userData['wsk_u'],10);
         $this->Log->log(0,"INTVAL WSK_U => ".$wsku);
-        if($wsku!==0)
-        {
+        if($wsku!==0){
             return false;
         }
         return true;
@@ -91,27 +95,25 @@ class ManagePermission
     private function checkPermissionExist()
     {
         $this->Log->log(0,"[".__METHOD__."] ID PERM => ".$this->idPerm);
-        $permData=$this->query('SELECT `id` FROM `uprawnienia` WHERE `id`=?',$this->idPerm);
-        $count=count($permData);
-        $this->Log->log(0,"COUNT => ".$count);
-        if($count===0)
-        {
-            $this->response->setError(1,"PERMISSION (".$this->idPerm."), NOT EXIST");
+        if(count($this->dbLink->squery('SELECT `id` FROM `uprawnienia` WHERE `id`=:i',[':i'=>[$this->idPerm,'INT']]))!==1){
+            Throw New Exception("PERMISSION (ID:".$this->idPerm.") DATABASE ERROR",1);
         }
     }
-    private function addUser($id,$user)
+    private function addPerm($id,$user)
     {
+        $this->Log->log(0,"[".__METHOD__."]");
         if(preg_match('/^user_(\d)+$/i', $id) && (intval($user,10)>0 ) && self::checkUserExist($user))
         {
             $this->Log->log(0,"INSERT => ".$user);
-            $this->query('INSERT INTO `uzyt_i_upr` (`id_uzytkownik`,`id_uprawnienie`) VALUES (?,?)',$user.",".$this->idPerm);    
-            return true;
+            $sql=[
+                ':iu'=>[$user,'INT'],
+                ':ip'=>[$this->idPerm,'INT']
+            ];
+            $this->dbLink->query('INSERT INTO `uzyt_i_upr` (`id_uzytkownik`,`id_uprawnienie`) VALUES (:iu,:ip)',$sql);    
         }
         else
         {
-            //$this->Log->log(0,"[".__METHOD__."] WRONG INPUT KEY (".$id."), OR WRONG ID USER (".$user.")");
-            $this->response->setError(1,"WRONG INPUT KEY (".$id."), OR WRONG ID USER (".$user.") OR USER WITH ID => ".$user." NOT EXIST OR USER IS DELETED");
-            return false;
+            Throw New Exception("WRONG INPUT KEY (".$id."), OR WRONG ID USER (".$user.") OR USER WITH ID => ".$user." NOT EXIST OR USER IS DELETED",1); 
         } 
     }
     # RETURN ALL NOT DELETED PROJECT FROM DB
@@ -125,7 +127,7 @@ class ManagePermission
         $result=$this->dbLink->squery('SELECT `ID` as \'i\',`SKROT` as \'s\',`NAZWA` as \'n\',`OPIS` as \'o\' FROM `uprawnienia` WHERE ID LIKE (:f) OR NAZWA LIKE (:f) OR SKROT LIKE (:f) OR OPIS LIKE (:f) ORDER BY ID asc'
                 ,$sql);
         // $this->dbLink->sth->fetch(PDO::FETCH_ASSOC)
-       echo json_encode($this->response->setResponse(__METHOD__,$result,'showAll','GET')); 
+       $this->utilities->jsonResponse(__METHOD__,$result,'showAll','GET');
     }
     public function getUsersWithPerm()
     {
@@ -138,7 +140,7 @@ class ManagePermission
         $v['u']=$this->dbLink->squery('SELECT id, ImieNazwisko FROM `v_upr_i_uzyt_v3` WHERE idUprawnienie=:i',[':i'=>[$id,'INT']]);
         // GET ALL USERS
         $v['a']=$this->dbLink->squery('SELECT ID as "id",CONCAT(Imie," ",Nazwisko) AS "ImieNazwisko" FROM `v_all_user` WHERE wskU=0 ORDER BY ID asc');
-        echo json_encode($this->utilities->getResponse(__METHOD__, $v,'uPermOff','POST'));
+        $this->utilities->jsonResponse(__METHOD__, $v,'uPermOff','POST');   
     }
     function __destruct(){}
 }

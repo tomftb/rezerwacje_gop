1a3450db76125639a40eef800102a7bf646de82f
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-07-07 14:29:49 +0200

    new modul

--- modul/ManageCluster.php
+++ modul/ManageCluster.php
@@ -13,18 +13,18 @@ class ManageCluster {
     public function getActClustrsUsage(){
         
         /* MERGE */
-        $this->Utilities->jsonResponse(__METHOD__,self::mergeLabClusters(),'','');
+        $v['labs']=self::getAllLabs();
+        $v['clusters']=self::getAllClusters();
+        $v['all']=self::mergeLabClusters($v['labs'],$v['clusters']);
+        $this->Utilities->jsonResponse(__METHOD__,$v,'','');
     }
     private function getAllClusters(){
-        return $this->db->squery('SELECT nod as \'n\',pracownia as \'p\' FROM klaster');
+        return $this->db->squery('SELECT id as \'i\',nod as \'n\',pracownia as \'p\' FROM klaster');
     }
     private function getAllLabs(){
         return $this->db->squery('SELECT id as \'i\',nazwa as \'n\' FROM pracownia WHERE WSK_U=0 ORDER BY id');
     }
-    private function mergeLabClusters(){
-        $clusters=self::getAllClusters();
-        $labs=self::getAllLabs();
-        
+    private function mergeLabClusters($labs,$clusters){
         foreach($labs as $l => $lab){
             
             $labs[$l]['c']='';
@@ -40,6 +40,40 @@ class ManageCluster {
         return $labs;
     }
     public function updateClustr(){
-        $this->Utilities->jsonResponse(__METHOD__,'','','');
+        $input=filter_input_array(INPUT_GET);
+        $this->Log->LogMulti(0,$input);
+        //array_walk([$this,'verifyUpdateKeys'],$input);
+
+        self::verifyUpdateKeys($input);
+        self::setIntKeys($input);   
+        /*
+         * TO DO CHECK NOD AND LAB EXIST IN DB
+         */
+        $parm=[
+            ':n0'=>[$input['n0'],'INT'],
+            ':n1'=>[$input['n1'],'INT'],
+            ':p'=>[$input['p'],'INT']
+        ];
+        if($input['n0']>$input['n1']){
+            $parm[':n0'][0]=$input['n1'];
+            $parm[':n1'][0]=$input['n0'];
+        }
+        //var_dump($input);
+        $this->Log->LogMulti(0,$parm,'PARM');
+        $this->db->setQuery("UPDATE `klaster` SET `pracownia`=:p WHERE `id`>=:n0 AND `id`<=:n1 ",$parm);
+        $this->db->runQuery();
+        //Throw New Exception ("test",0);
+        $this->Utilities->jsonResponse(__METHOD__,['all'=>self::mergeLabClusters(self::getAllLabs(),self::getAllClusters())],'','');
+    }
+    private function verifyUpdateKeys($input){
+        $this->Utilities->validateKey($input,'n0',true,1);
+        $this->Utilities->validateKey($input,'n1',true,1);
+        $this->Utilities->validateKey($input,'p',true,1);
+        $this->Log->LogMulti(0,$input,'AFTER');
+    }
+    private function setIntKeys(&$input){
+        $input['n0']=$this->Utilities->getNumber($input['n0']);
+        $input['n1']=$this->Utilities->getNumber($input['n1']);
+        $input['p']=$this->Utilities->getNumber($input['p']);
     }
 }

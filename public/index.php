4107b2a16be3be73c4d334e68e80025429598da8
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-07-08 11:52:32 +0200

    add try catch block

--- public/index.php
+++ public/index.php
@@ -4,7 +4,18 @@ define('DR',filter_input(INPUT_SERVER,"DOCUMENT_ROOT").'/..');
 require(DR.'/.cfg/consts.php');
 require(DR.'/.cfg/config.php');
 require(DR."/function/autoLoader.php");
-/* 
- * STATIC ABSTRACT FACTORY
- */
-PageManager::load(filter_input(INPUT_GET,"id", FILTER_VALIDATE_INT));
+try{
+    /* 
+    * STATIC ABSTRACT FACTORY
+    */
+    PageManager::load(filter_input(INPUT_GET,"id", FILTER_VALIDATE_INT));
+}
+catch(Throwable $t){
+    echo $t->getMessage()."<br/>";
+    echo $t->getCode()."<br/>";
+}
+finally {
+         
+} 
+
+

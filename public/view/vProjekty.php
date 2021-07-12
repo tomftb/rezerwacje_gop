ee7faadceb3d282300e39a0981c30589a65eb316
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-07-12 15:18:27 +0200

    fix label to Projekty

--- public/view/vProjekty.php
+++ public/view/vProjekty.php
@@ -1,5 +1,5 @@
 <?php if(!defined("APP_URL")){ die('Direct access not permitted');} ?>
-<div class="container-fluid pl-0 pr-5 mr-5" style="margin-top:-55px; position:fixed; z-index:996;border:0px solid green;" id="mainHead"> 
+<div class="container-fluid pl-0 pr-5 mr-5" style="margin-top:-55px; position:fixed; z-index:996;" id="mainHead"> 
     <div class="row">
         <div class="col-4" >
             <div class="btn pull-left mt-0" > 
@@ -7,14 +7,14 @@
             </div> 
         </div>
         <div class="col-4" >
-            <h2 class="text-center mb-3 mt-1 text-info">Pracownicy :</h2>
+            <h2 class="text-center mb-3 mt-1 text-info">Projekty :</h2>
         </div>
-        <div class="col-4" style="border:0px solid pink;">
-            <div class="row float-right w-100 mr-0" style="border:0px solid purple;">
-                <div class="col-4 pr-0" style="border:0px solid black;">
-                    <h5 class="mt-3 text-secondary text-right"style="border:0px solid black;" >Szukaj :</h5>
+        <div class="col-4">
+            <div class="row float-right w-100 mr-0">
+                <div class="col-4 pr-0">
+                    <h5 class="mt-3 text-secondary text-right">Szukaj :</h5>
                 </div>
-                <div class="col-8 pl-0 pr-0" style="border:0px solid red;">
+                <div class="col-8 pl-0 pr-0">
                     <input class="ml-1 form-control mt-2"  onchange="findData(this.value)"/>
                 </div>
             </div>

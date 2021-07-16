d01ee059fa5058acae3dd99ea5ad6a5067ce0046
Tomek Home <tborczynski87@gmail.com>
2021-07-16 11:59:50 +0200

    fix FileExtensionContentType add dir var

--- class/downloadFile.php
+++ class/downloadFile.php
@@ -3,20 +3,20 @@
 final class downloadFile{
     //put your code here
     private static $FileExtensionContentType=[
-        'docx'=>['application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
-        'pdf'=>['application/pdf'],
-        'jpg'=>['image/jpeg'],
-        'jpeg'=>['image/jpeg'],
-        'bmp'=>['image/bmp'],
-        'gif'=>['image/gif'],
-        'png'=>['image/png']
+        'docx'=>'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'pdf'=>'application/pdf',
+        'jpg'=>'image/jpeg',
+        'jpeg'=>'image/jpeg',
+        'bmp'=>'image/bmp',
+        'gif'=>'image/gif',
+        'png'=>'image/png'
     ];
     private function __construct(){}
-    public static function getFile($file){
+    public static function getFile($dir,$file){
         
-        self::setUpHeader(self::parseFile($file));
+        self::setUpHeader(self::parseFile($dir,$file));
     }
-    private function parseFile($file=''){
+    private function parseFile($dir='',$file=''){
         if(trim($file)===''){
            die('NO FILE INPUT'); 
         }
@@ -28,19 +28,22 @@ final class downloadFile{
         if(!array_key_exists($ext, self::$FileExtensionContentType)){
             die('NOT AVAILABLE FILE EXTENSION'); 
         }
-        self::checkFileExist($file);
-        self::checkFileReadable($file);
-        return [$file,self::$FileExtensionContentType[$ext]];
+        self::checkFileExist($dir.$file);
+        self::checkFileReadable($dir.$file);
+        return [$dir,$file,self::$FileExtensionContentType[$ext]];
     }
     private function setUpHeader($file=[]){
         
-        if(!$file[1] || !$file[0]){
+        if(!$file[1] || !$file[0] || !$file[2]){
             var_dump($file);
             die('WRONG FILE'); 
-        }
-        header('Content-Type: '.$file[1]);
-        header("Content-Disposition:attachment;filename=".$file[0]."");
-        readfile($file[0]);
+        }       
+        header('Content-Type: '.$file[2]);
+        header('Content-Length: '.filesize($file[0].$file[1]));
+        //header("Content-Disposition:attachment;filename=\"".basename($file[1])."\"");
+        readfile($file[0].$file[1]);
+        //print file_get_contents($file[0].$file[1]);
+         
     }
     private function checkFileExist($file){
         if(!file_exists($file)){

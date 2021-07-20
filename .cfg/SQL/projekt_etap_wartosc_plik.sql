a65b0951fa48d87256482ccf931bf5e45d108c8f
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2021-07-20 13:02:46 +0200

    add AI

--- .cfg/SQL/projekt_etap_wartosc_plik.sql
+++ .cfg/SQL/projekt_etap_wartosc_plik.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Czas generowania: 16 Lip 2021, 10:18
--- Wersja serwera: 10.4.17-MariaDB
--- Wersja PHP: 7.2.34
+-- Host: localhost
+-- Czas generowania: 20 Lip 2021, 13:02
+-- Wersja serwera: 5.7.20-0ubuntu0.16.04.1
+-- Wersja PHP: 7.0.33-0ubuntu0.16.04.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `projekt_etap_wartosc_plik` (
   `id` int(11) NOT NULL,
   `idProjectStageValue` int(11) NOT NULL,
-  `fileId` int(11) NOT NULL DEFAULT 0,
+  `fileId` int(11) NOT NULL DEFAULT '0',
   `filename` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `fileposition` varchar(20) COLLATE utf8_polish_ci NOT NULL,
   `originalname` varchar(200) COLLATE utf8_polish_ci NOT NULL,
@@ -50,18 +49,28 @@ CREATE TABLE `projekt_etap_wartosc_plik` (
   `mod_host` varchar(100) COLLATE utf8_polish_ci DEFAULT NULL,
   `wsk_u` enum('0','1') COLLATE utf8_polish_ci NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indeksy dla tabeli `projekt_etap_wartosc_plik`
+-- Indexes for table `projekt_etap_wartosc_plik`
 --
 ALTER TABLE `projekt_etap_wartosc_plik`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_projekt_etap3_projekt_id` (`idProjectStageValue`),
   ADD KEY `FK_projekt_etap3_cuser_id` (`create_user_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `projekt_etap_wartosc_plik`
+--
+ALTER TABLE `projekt_etap_wartosc_plik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Ograniczenia dla zrzutów tabel
 --
@@ -72,7 +81,6 @@ ALTER TABLE `projekt_etap_wartosc_plik`
 ALTER TABLE `projekt_etap_wartosc_plik`
   ADD CONSTRAINT `FK_projekt_etap3_cuser_id` FOREIGN KEY (`create_user_id`) REFERENCES `uzytkownik` (`id`),
   ADD CONSTRAINT `FK_projekt_etap3_projekt_id` FOREIGN KEY (`idProjectStageValue`) REFERENCES `projekt_etap_wartosc` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

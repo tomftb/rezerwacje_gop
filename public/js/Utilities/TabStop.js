001a6f115adc7cd5ee960f871e37f5e6ee656cfe
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2022-06-14 14:38:54 +0200

    new v

--- public/js/Utilities/TabStop.js
+++ public/js/Utilities/TabStop.js
@@ -52,8 +52,11 @@ class TabStop{
             console.log(arr);*/
            
         //});
+        var lp=0;
         for(var index in this.data){
-            dynamicDiv.appendChild(self.createInputRow(index,this.data));
+            
+            dynamicDiv.appendChild(self.createInputRow(index,this.data,lp));
+            lp++;
         }
     }
     getData(){
@@ -79,32 +82,39 @@ class TabStop{
             div.appendChild(text);
             /* CLOSURE */
             div.onclick = function(){
-                var data = self.getDefault();
+                console.log('TabStop::createAddButton');
+                //var data = self.getDefault();
                 //var idx =  self.Utilities.countObjectProp(self.data);  
-                var idx =  parseInt(self.Utilities.getLastProp(self.data),10)+1; 
+                var newIdx =  parseInt(self.Utilities.getLastProp(self.data),10)+1; 
+                var lp = self.Utilities.countObjectProp(self.data);
                     //self.data.push(data);
+                    console.log('Data:');
                     console.log(self.data);
-                    console.log(idx);
+                    console.log('IDX:');
+                    console.log(newIdx);
+                    console.log('Option:');
                     console.log(self.option);
-                var option=self.Html.createOption();
-                    option.setAttribute('value',idx);
-                    //option.setAttribute('class',data[property].fontcolor+' '+data[property].backgroundcolor);
-                    //option.style.fontFamily = data.fontFamily;
-                    //option.style.color = data.color;
-                    //option.style.backgroundColor = data.backgroundcolor;
-                    option.innerText=idx; 
+                    console.log('Lp:');
+                    console.log(lp);
+                    self.data[newIdx] = self.getDefault();
+                var newOption=self.Html.createOption();
+                    newOption.setAttribute('value',newIdx);
+                     
                     //throw 'llll';
-                    self.data[idx] = self.getDefault();
+                    
+                    //all[prop]=this.Utilities.getDefaultOptionProperties(prop,subsectionrow.paragraph.tabStop[prop].position+' '+subsectionrow.paragraph.tabStop[prop].measurementName+' | '+subsectionrow.paragraph.tabStop[prop].alignmentName+' | '+subsectionrow.paragraph.tabStop[prop].leadingSignName);
+                    newOption.innerText = self.setOptionLabal(self.data[newIdx]);
+                    //newOption.innerText=self.data[newIdx].position+' '+self.data[newIdx].measurementName+' | '+self.data[newIdx].alignmentName+' | '+self.data[newIdx].leadingSignName; 
                     //throw 'aaaaaaaaa';
-                    self.option.appendChild(option);
-                    dynamicDiv.appendChild(self.createInputRow(idx,self.data));
+                    self.option.appendChild(newOption);
+                    dynamicDiv.appendChild(self.createInputRow(newIdx,self.data,lp));
                 /* ADD ROW tabstop PROPERTY */
               
                 
             };
         return div;
     }
-    createInputRow(idx,data){
+    createInputRow(idx,data,lp){
         console.log('TabStop::createInputRow()');
         console.log(idx);
         console.log(data);
@@ -113,63 +123,73 @@ class TabStop{
         var divTool = document.createElement('DIV');
             divTool.classList.add('input-group');
             /* VALUE INPUT */
-            divTool.appendChild(this.createValue(data[idx],'position'));
+            divTool.appendChild(this.createValue(data[idx],'position',lp));
             /* MEASUREMENT SELECT */
-            divTool.appendChild(this.createInputRowProperty(data[idx],'measurementName','measurement',this.property.listMeasurement));
+            divTool.appendChild(this.createInputRowProperty(data[idx],'measurementName','measurement',this.property.listMeasurement,lp));
             /* ALIGN SELECT */
-            divTool.appendChild(this.createInputRowProperty(data[idx],'alignmentName','alignment',this.property.tabStopAlign));
+            divTool.appendChild(this.createInputRowProperty(data[idx],'alignmentName','alignment',this.property.tabStopAlign,lp));
             /* LEADING SIGN SELECT */
-            divTool.appendChild(this.createInputRowProperty(data[idx],'leadingSignName','leadingSign',this.property.leadingSign));
+            divTool.appendChild(this.createInputRowProperty(data[idx],'leadingSignName','leadingSign',this.property.leadingSign,lp));
             /* REMOVE BUTTON */
-            divTool.appendChild(this.createRemoveButton(divTool,idx,data));
+            divTool.appendChild(this.createRemoveButton(divTool,idx,data,lp));
        return divTool;
     }
-    createValue(data,key){
+    createValue(data,key,lp){
         /* INPUT - Pozycja tabulatora */
+        var self = this;
         var input=document.createElement('INPUT');
             input.classList.add('form-control-sm','form-control');
             input.setAttribute('type','number');
             /* TO FIX */
             input.setAttribute('value',data[key]);
             input.onchange = function(){
-                data[key]=this.value;
+                data[key]=parseFloat(this.value);
+                self.option.childNodes[lp].childNodes[0].remove();
+                self.option.childNodes[lp].appendChild(document.createTextNode(self.setOptionLabal(data)));
                 console.log(data);
             };
             return input;
     }
-    createInputRowProperty(data,name,value,all){
+    createInputRowProperty(data,nameKey,valueKey,glossary,lp){
         var select=document.createElement('SELECT');
             select.classList.add('form-control-sm','form-control');
         var defaultOption={
-                0:this.Utilities.getDefaultOptionProperties(data[value],data[name])
+                0:this.Utilities.getDefaultOptionProperties(data[valueKey],data[nameKey])
             };
+        var self = this;
             select.appendChild(this.Html.createOptionGroup('Domyślny:',defaultOption)); 
-            select.appendChild(this.Html.createOptionGroup('Dostępne',this.Utilities.getDefaultList(all,data[value])));
+            select.appendChild(this.Html.createOptionGroup('Dostępne',this.Utilities.getDefaultList(glossary,data[valueKey])));
             select.onchange=function(){
-            //console.log(this.value); 
-            //console.log(this.selectedIndex); 
-            //console.log(this.name);  
-            data[value]=this.value;
-            console.log(data);
-            //console.log(data);  
-            //stageData.departmentName = data[this.selectedIndex]; 
+                /* UPDATE OPTION SELECT VALUE IN DATA TABSTOP OBJECT*/
+                data[valueKey]=this.value;
+                 /* UPDATE OPTION SELECT LABEL IN DATA TABSTOP OBJECT*/
+                data[nameKey]=self.getValueNameFromGlossary(glossary,this.value);;
+                /* REMOVE OPTION SELECT WITH OLD DATA VALUE*/
+                self.option.childNodes[lp].childNodes[0].remove();
+                /* APPEND NEW OPTION SELECT WITH NEW DATA VALUE*/
+                self.option.childNodes[lp].appendChild(document.createTextNode(self.setOptionLabal(data)));
+
             };
             
             
         return select;   
     }
-    createRemoveButton(divRow,idx,data){
+    createRemoveButton(divRow,idx,data,lp){
         var div = this.Html.removeButton();
             console.log(div);
             div.classList.add('input-group-text');
         var divAll = document.createElement('div');    
             divAll.classList.add('input-group-append');
             /* CLOSURE */
+        var self = this;
             div.onclick=function(){
-                if (confirm('Potwierdź usunięcie tabulacji') === true) {
+                if (confirm('Potwierdź usunięcie tabulacji') === true) {   
+                    /* REMOVE DIV ROW WITH INPUT AND BUTTON */
                     divRow.remove();
+                    /* REMOVE DATA IDX FROM DATA (LIST) TABSTOP OBJECT */
                     delete data[idx];
-                    //delete
+                    /* REMOVE VALUE OPTION FROM LIST IN SELECT TABSTOP */
+                    self.option.childNodes[lp].remove();
                 } else {
                     // NOTHING TO DO
                 }
@@ -179,5 +199,16 @@ class TabStop{
     }
     setOption(option){
         this.option=option;
+    }
+    setOptionLabal(d){
+        return d.position+' '+d.measurementName+' | '+d.alignmentName+' | '+d.leadingSignName; 
+    }
+    getValueNameFromGlossary(glossary,key){
+        for(const prop in glossary){
+            if(glossary[prop].v===key){
+                return glossary[prop].n;
+            }
+        }
+        return 'NOT FOUND';
     }
 }

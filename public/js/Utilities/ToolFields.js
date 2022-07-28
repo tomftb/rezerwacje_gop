9f0b900a03c12b4dd2c8b4a84875930323cf1322
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2022-07-28 09:51:52 +0200

    change proeprty name Tool to Field

--- public/js/Utilities/ToolFields.js
+++ public/js/Utilities/ToolFields.js
@@ -1,6 +1,6 @@
 class ToolFields{
     
-    Tool = new Object();
+    Field = new Object();
     Main = new Object();
     Html = new Object();
     
@@ -8,23 +8,23 @@ class ToolFields{
         this.Html = new Html();
         this.Main=this.Html.getRow();
         for(var i= 0;i<Size.length;i++){     
-            this.Tool[i]=this.Html.getCol(Size[i]);
+            this.Field[i]=this.Html.getCol(Size[i]);
             this.Main.appendChild(this.Tool[i]);
         }
     }
     get(prop){
         this.check(prop);
-        return this.Tool[prop];
+        return this.Field[prop];
     }
     getMain(){
         return this.Main;
     }
     set(prop,ele){
         this.check(prop);
-        this.Tool[prop].appendChild(ele);
+        this.Field[prop].appendChild(ele);
     }
     check(prop){
-        if(!this.Tool.hasOwnProperty(prop)){
+        if(!this.Field.hasOwnProperty(prop)){
             throw 'ToolFields::set() Error - proeprty not exists';
         }
     }

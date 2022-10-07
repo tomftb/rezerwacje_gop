7354a3a580a8ef9a3baccaadc092926985900446
tborczynski <tborczynski@DDI1374TB.geofizyka.pl>
2022-10-07 08:35:12 +0200

    update preapre() change setChangeDataState() this.Table to this and change run to setResponse()

--- public/js/Page/ProjectStage.js
+++ public/js/Page/ProjectStage.js
@@ -61,7 +61,7 @@ class ProjectStage{
             };
             //this.Items.setCloseModal(this,'show',this.defaultTask+data['data']['value']['stage'].i);
             this.Items.setCloseModal(run);
-            this.Items.setChangeDataState(data['data']['value']['stage'].i,data['data']['value']['stage'].t,data['data']['function'],data['data']['value']['slo'],btnLabel,btnClass,this.StageTable,'run',window.router+this.defaultTask+data['data']['value']['stage'].i);
+            this.Items.setChangeDataState(data['data']['value']['stage'].i,data['data']['value']['stage'].t,data['data']['function'],data['data']['value']['slo'],btnLabel,btnClass,this,'setResponse',this.Items.router+this.defaultTask+data['data']['value']['stage'].i);
             this.Items.Modal.setInfo("Project Stage ID: "+data['data']['value']['stage'].i+", Create user: "+data['data']['value']['stage'].cu+" ("+data['data']['value']['stage'].cul+"), Create date: "+data['data']['value']['stage'].cd);
        
     }
